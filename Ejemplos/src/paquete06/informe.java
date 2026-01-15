b1d1e1c28690b757e2ea5e8dcd1efed1238866ee
teoperalta <teo.peralta.2007@gmail.com>
2026-01-15 11:17:32 -0500

    Informe

---
+++ Ejemplos/src/paquete06/informe.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class informe {
+    
+    public static void imprimir(int a, int b, int c, int d){
+          System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a,b,
+                c,
+                a, b,
+                d);
+    }
+}
